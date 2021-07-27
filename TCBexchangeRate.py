4ddbb80db24635aee188e3c1f0e6ed5ac4ed307c
chiache Lee <chiache7410@gmail.com>
2021-07-28 02:57:24 +0800

    add date

--- TCBexchangeRate.py
+++ TCBexchangeRate.py
@@ -1,5 +1,6 @@
 import urllib.request as req
 import bs4
+import datetime
 def web_crawler(url):
     request=req.Request(url, headers={
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
@@ -13,9 +14,10 @@ def web_crawler(url):
     return([currency, exchangerate])
 
 def write_file(filenaem, lines):
+    filenaem = filenaem + '.csv'
     with open(filenaem, 'a', encoding='utf-8') as f:
         #f.write('日期' + ',' + '匯率' + '\n')
-        f.write(lines[0] + ',' + lines[1] + '\n')
+        f.write(str(datetime.date.today()) + ',' + lines[1] + '\n')
 
 def main():
     #lines = ['20210722' + ',' + '30.1' + '\n' + '20210723' + ',' + '29.8' + '\n']
@@ -23,7 +25,7 @@ def main():
     #抓取TCB官網上美元的即期買入匯率
     d = web_crawler('https://ibank.tcbbank.com.tw/PIB/cb5/cb501005/CB501005_01.faces')
     print(d)
-    write_file('usd.csv', d)
+    write_file(d[0], d)
     print('OK')
 
 if __name__ == '__main__':
